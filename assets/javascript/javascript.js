108df901abcc72fbe8e6f0821a3bbf081d257d27
intaglioplate312 <helenelizabeth@me.com>
2017-06-16 07:34:43 -0500

    and so it begins...basichtml layout and css, start of pseudo code

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,57 @@
+//Starting variables
+//Letter choices available
+// $(function () {
+
+// access initailize firebase
+// config firebase
+// == firebase.database
+
+// access moments.js
+
+// onclick function to send grab data from form
+
+// input-trainName
+
+// input-trainDestination
+
+// input-trainDeparture
+
+// input-trainFrequency
+
+// make an object to send to database == push?
+
+// onclick function to send data to firebase simulatiously display
+// update/snapshot function
+
+// clear form  
+// return false or default...
+
+
+// Time functions moment.js
+
+// grab reat time and diplay
+// calculate departure times from start + frequesncy
+// caluclate (next departure time - real time == remaining minutes to next train)
+
+
+
+
+// add to table in rows or body...
+
+// display-trainName
+
+// display-trainDestination
+
+// display-trainDeparture
+
+// display-trainFrequency
+
+
+// display- calutation of time till next departure
+
+//});
+
+
+
+
+
