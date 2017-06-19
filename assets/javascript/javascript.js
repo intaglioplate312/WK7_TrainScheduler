bc0bd9849373941a0045130199f772727010f5d8
intaglioplate312 <helenelizabeth@me.com>
2017-06-18 20:02:12 -0500

    not validate or linted, broke my minutes, fixed that broke my refresh, oh well. Turn in and fix when I can

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,9 +1,3 @@
-//Starting variables
-//Letter choices available
-// $(function () {
-
-// access initailize firebase
-
 // Initialize Firebase
 var config = {
     apiKey: "AIzaSyBlaf1ST7wUoySRa8Wf6kKP8kf53G2r2KY",
@@ -20,6 +14,11 @@ firebase.initializeApp(config);
 
 var dataRef = firebase.database();
 
+// Initial Values
+var name = "";
+var destination = "";
+var start = 0;
+var frequency = "";
 
 
 // // onclick function to send grab data from form
@@ -33,7 +32,7 @@ $("#addTrain").on("click", function(event) {
     // input-trainFrequency
     name = $("#name-input").val().trim();
     destination = $("#destination-input").val().trim();
-    start = $("#start-input").val().trim();
+    start = moment($("#start-input").val().trim(), "HH:mm").subtract(10, "years").format("X");
     frequency = $("#frequency-input").val().trim();
 
     // send to database == push
@@ -45,46 +44,108 @@ $("#addTrain").on("click", function(event) {
         frequency: frequency,
         dateAdded: firebase.database.ServerValue.TIMESTAMP
     });
+
+    //clear inputs
+    $("#name-input").val("");
+    $("#destination-input").val("");
+    $("#start-input").val("");
+    $("#frequency-input").val("");
+    return false;
 });
 
 // update/snapshot function
 dataRef.ref().on("child_added", function(childSnapshot) {
+
+    //waited for Panotop video, but never uploaded
+    //timer differentials ( not ready for prime time!!)
+
+    console.log(childSnapshot.val());
+
+    var mfrequency = childSnapshot.val().frequency
+
+    var nextTrain = childSnapshot.val().start
+
+    var timeDifference = moment().diff(moment.unix(nextTrain), "minutes");
+
+    var remainingTime = moment().diff(moment.unix(nextTrain), "minutes") % mfrequency;
+
+    var timeMinutes = mfrequency - remainingTime;
+
+    var arrivalTime = moment().add(timeMinutes, "m").format("hh:mm A");
+    
+    console.log(frequency)
+    console.log(nextTrain)
+    console.log(timeDifference)
+    console.log(remainingTime)
+    console.log(moment().format("hh:mm A"))
+    console.log(timeDifference);
+    console.log(moment().format("X"))
+    console.log(remainingTime)
+    console.log(timeMinutes)
+    console.log(moment().format("hh:mm A"))
+    console.log(arrivalTime);
+    console.log(moment().format("X"))
 
 
     // display-trainName
     // display-trainDestination
     // display-trainDeparture
     // display-trainFrequency
-
     $("#trainList").append("<div class='well'><div id='name'> " + childSnapshot.val().name +
-        " </div><div id='destination'> " + childSnapshot.val().destination +
-        " </div><div id='frequency'> " + childSnapshot.val().frequency +
-        " </div><div id='start'> " + childSnapshot.val().start +
-        " <div id='dateAdded'> " + childSnapshot.val().dateAdded + " </div></div>");
-
+        " </div><div id='destination'> " + "Destination " + childSnapshot.val().destination +
+        " </div><div id='frequency'> " + "runs every " + childSnapshot.val().frequency + " minutes" +
+        " </div><div id='start'> " + "Arriving in  " + timeMinutes + " minutes" +
+        " </div><div id='dateAdded'> " + "At  " + arrivalTime + " </div></div>");
 
     // Handle the errors
 }, function(errorObject) {
     console.log("Errors handled: " + errorObject.code);
 });
 
+dataRef.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", function(snapshot) {
+    // Change the HTML to reflect
+    $("#name-display").html(snapshot.val().name);
+    $("#destination-display").html(snapshot.val().destination);
+    $("#start-display").html(snapshot.val().start);
+    $("#frequency-display").html(snapshot.val().frequency);
 
+});
 
+//set current time if analog clock wont work
+// function displayTime() {
+//     //var time = moment().format('hh:mm:ss a');
+//     var timeNow = moment().format('h:mm:ss a');
+//     $('.time').html("Apparently it's:  " + timeNow);
+//     setTimeout(displayTime, 1000);
+// };
+//  displayTime();
 
-// clear form  
-// return false or default...
+const HOURHAND = document.querySelector("#hour");
+const MINUTEHAND = document.querySelector("#minute");
+const SECONDHAND = document.querySelector("#second");
 
+var date = new Date();
+console.log(date);
+let hr = date.getHours();
+let min = date.getMinutes();
+let sec = date.getSeconds();
+console.log("Hour: " + hr + " Minute: " + min + " Second: " + sec);
 
-// Time functions moment.js
+let hrPosition = (hr*360/12)+(min*(360/60)/12);
+let minPosition = (min*360/60)+(sec*(360/60)/60);
+let secPosition = sec*360/60;
 
-// grab reat time and diplay
-// calculate departure times from start + frequesncy
-// caluclate (next departure time - real time == remaining minutes to next train)
+function runTheClock() {
 
+    hrPosition = hrPosition+(3/360);
+    minPosition = minPosition+(6/60);
+    secPosition = secPosition+6;
 
+    HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
+    MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
+    SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
 
+}
 
+var interval = setInterval(runTheClock, 1000);
 
-// display- calutation of time till next departure
-
-//});
